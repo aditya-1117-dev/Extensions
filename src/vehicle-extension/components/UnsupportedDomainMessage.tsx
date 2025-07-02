1652efa9569d814dbb6eed246fd33c4b53d6a641
Manan-Shukla-Noovosoft <manan.shukla@noovosoft.com>
2025-07-02 17:27:05 +0530

    Feat: add unsupported domain component

---
+++ src/vehicle-extension/components/UnsupportedDomainMessage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UnsupportedDomainMessage: React.FC = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-[150px] bg-white text-red-600 px-4">
+            <div className="max-w-md w-full p-6 rounded-lg border border-red-300 shadow-md bg-red-50">
+                <h1 className="text-xl font-bold mb-2">Unsupported Domain</h1>
+                <p className="text-sm mb-1">You are not on the Twinntax website.</p>
+            </div>
+        </div>
+    );
+};
+
+export default UnsupportedDomainMessage;
